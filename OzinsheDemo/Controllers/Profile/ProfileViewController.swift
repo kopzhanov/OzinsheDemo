be807a7488081db7c5e761fca408a54ccb161264
Alikhan Kopzhanov <w1nt3r@Alikhans-MBP.KopKingdom>
2024-09-03 16:25:13 +0500

    Added localization on profile screen + Dark Mode switch(need help)

--- OzinsheDemo/Controllers/Profile/ProfileViewController.swift
+++ OzinsheDemo/Controllers/Profile/ProfileViewController.swift
@@ -13,6 +13,13 @@ class ProfileViewController: UIViewController, LanguageProtocol {
     @IBOutlet weak var myProfileLabel: UILabel!
     @IBOutlet weak var languageButton: UIButton!
     @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var personalDataButton: UIButton!
+    @IBOutlet weak var changePasswordButton: UIButton!
+    @IBOutlet weak var termsAndConditionsButton: UIButton!
+    @IBOutlet weak var notificationsButton: UIButton!
+    @IBOutlet weak var darkModeButton: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +33,16 @@ class ProfileViewController: UIViewController, LanguageProtocol {
     
     
     func configureViews(){
+        self.navigationItem.title = "PROFILE".localized()
         myProfileLabel.text = "MY_PROFILE".localized()
+        personalDataButton.setTitle("PERSONAL_DATA".localized(), for: .normal)
+        changePasswordButton.setTitle("CHANGE_PASSWORD".localized(), for: .normal)
+        termsAndConditionsButton.setTitle("TERMS_AND_CONDITIONS".localized(), for: .normal)
+        notificationsButton.setTitle("NOTIFICATIONS".localized(), for: .normal)
+        darkModeButton.setTitle("DARK_MODE".localized(), for: .normal)
+        
         languageButton.setTitle("LANGUAGE".localized(), for: .normal)
+        
         if Localize.currentLanguage() == "en"{
             languageLabel.text = "English"
         }
@@ -71,6 +86,15 @@ class ProfileViewController: UIViewController, LanguageProtocol {
     func languageDidChange() {
         configureViews()
     }
+    
+    
+//    @IBAction func switcherChanged(_ sender: UISwitch) {
+//        if sender.isOn{
+//            super.overrideUserInterfaceStyle = .dark
+//        } else {
+//            super.overrideUserInterfaceStyle = .light
+//        }
+//    }
     /*
     // MARK: - Navigation
 
